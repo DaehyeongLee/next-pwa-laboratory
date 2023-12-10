593db351b86c4918d9983012cac7990578a6df65
DaehyeongLee <apm5018@naver.com>
2023-12-11 00:17:45 +0900

    다국어 적용을 위한 serversideprops 적용

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,17 @@
 import { useTranslation } from 'next-i18next'
+import { GetServerSideProps, GetServerSidePropsContext } from 'next'
+import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
 
 export default function Page() {
-  const { t } = useTranslation('ko')
+  const { t } = useTranslation('common')
   return <h1>{t('greeting')}</h1>
 }
+
+
+export const getServerSideProps: GetServerSideProps = async (ctx: GetServerSidePropsContext) => {
+  return {
+    props: {
+      ...(await serverSideTranslations(ctx.locale ?? 'ko', ['common'])),
+    },
+  }
+};
