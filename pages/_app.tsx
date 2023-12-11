8b1e1b449f4df912efcf5f382f68e4e4cb2647a3
DaehyeongLee <apm5018@naver.com>
2023-12-11 16:17:22 +0900

    mui theme 적용

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,15 @@
 import { AuthProvider } from '@/components/context/AuthContext'
 import { appWithTranslation } from 'next-i18next'
+import { ThemeProvider } from '@mui/material/styles'
+import theme from '../lib/theme'
 import type { AppProps } from 'next/app'
- 
+
 const App = ({ Component, pageProps }: AppProps) => {
   return (
     <AuthProvider>
-      <Component {...pageProps} />
+      <ThemeProvider theme={theme}>
+        <Component {...pageProps} />
+      </ThemeProvider>
     </AuthProvider>
   )
 }
