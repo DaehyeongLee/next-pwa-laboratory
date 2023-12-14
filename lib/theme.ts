a5431b26210f06140e203265c8cc4768c363767d
DaehyeongLee <apm5018@naver.com>
2023-12-14 21:08:39 +0900

    테마색을 적용

--- lib/theme.ts
+++ lib/theme.ts
@@ -3,7 +3,42 @@ import { createTheme } from '@mui/material/styles'
 const appTheme = (mode: 'light' | 'dark') => createTheme({
   palette: {
     mode: mode,
+    common: {
+      black: '#333333',
+      white: '#ffffff',
+    },
+    primary: {
+      main: '#7545cd',
+      light: '#ab7ffc',
+      dark: '#441899',
+      contrastText: '#ffffff',
+    },
+    secondary: {
+      main: '#999999',
+    },
   },
+  typography: {
+    fontFamily: [
+      'Pretendard',
+      '-apple-system',
+      'BlinkMacSystemFont',
+      'system-ui',
+      'Roboto',
+      '"Helvetica Neue"',
+      '"Segoe UI"',
+      '"Apple SD Gothic Neo"',
+      '"Noto Sans KR"',
+      '"Malgun Gothic"',
+      '"Apple Color Emoji"',
+      '"Segoe UI Emoji"',
+      '"Segoe UI Symbol"',
+      'sans-serif',
+    ].join(','),
+    fontSize: 16,
+    fontWeightBold: 700,
+    fontWeightMedium: 600,
+    fontWeightRegular: 400,
+  }
 })
 
 export default appTheme
