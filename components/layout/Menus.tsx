0e6f2283431be278d7c99e07c03df0e4243cea04
DaehyeongLee <apm5018@naver.com>
2023-12-25 23:22:16 +0900

    Drawer에 메뉴 항목 추가

--- components/layout/Menus.tsx
+++ components/layout/Menus.tsx
@@ -1,18 +1,56 @@
 import React, { FC } from 'react'
-import { Drawer } from '@mui/material'
+import { Box, Drawer, ListItemButton, Typography, ListItemIcon } from '@mui/material'
 import { useTranslation } from 'next-i18next'
+import HomeIcon from '@mui/icons-material/Home'
 
 interface MenuListProps {
   open: boolean
   onClose(): void
 }
 
+const menuList = [
+  {
+    href: '/',
+    page: 'home',
+    icon: <HomeIcon />,
+  },
+]
+
 const Menus: FC<React.PropsWithChildren<MenuListProps>> = ({ open, onClose }) => {
   const { t } = useTranslation('common')
 
   return (
-    <Drawer anchor="left" open={open} onClose={onClose}>
-      {t('menu')}
+    <Drawer
+      anchor="left"
+      open={open}
+      onClose={onClose}
+      sx={{
+        '& .MuiDrawer-paper': {
+          boxSizing: 'border-box',
+          width: '312px',
+          borderTopRightRadius: 12,
+          borderBottomRightRadius: 12,
+        },
+      }}
+    >
+      <Box>
+        {
+          menuList.map(({ href, page, icon }) => (
+            <ListItemButton
+              key={page}
+              href={href}
+              sx={{
+                height: '56px',
+              }}
+            >
+              <ListItemIcon>{icon}</ListItemIcon>
+              <Typography variant="body1">
+                {t(page)}
+              </Typography>
+            </ListItemButton>
+          ))
+        }
+      </Box>
     </Drawer>
   )
 }
