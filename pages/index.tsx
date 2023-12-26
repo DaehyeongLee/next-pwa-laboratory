398170a5045fc0cf33a999f53dd11a2790643ccb
DaehyeongLee <apm5018@naver.com>
2023-12-26 22:12:08 +0900

    홈에 carousel 추가

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,49 @@
 import { useTranslation } from 'next-i18next'
 import { GetServerSideProps, GetServerSidePropsContext } from 'next'
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
-import { Typography } from '@mui/material'
+import { Typography, Box } from '@mui/material'
+import Carousel from "react-material-ui-carousel"
+
+const stepsCarousel = [
+  {
+    title: "carousel1",
+    url: 'images/index/carousel1.jpg',
+    desc: [
+      "멋진 옷과 슈트케이스를 확인하세요.",
+    ],
+  },
+  {
+    title: "carousel2",
+    url: 'images/index/carousel2.jpg',
+    desc: [
+      "자연광을 듬뿍 받은 옷을 확인하세요.",
+    ],
+  },
+]
 
 export default function Page() {
   const { t } = useTranslation('common')
-  return <Typography variant='h3'>{t('greeting')}</Typography>
+  return (
+    <>
+      <Carousel
+        autoPlay={false}
+        navButtonsAlwaysVisible
+        sx={{
+          width: '700px'
+        }}
+      >
+        {stepsCarousel.map((content) => (
+          <Box justifyContent="center">
+            <img src={content.url} style={{ width: '700px', height: '300px' }} />
+            {content.desc.map((description) => (
+              <li>{description}</li>
+            ))}
+          </Box>
+        ))}
+      </Carousel>
+      <Typography variant='h3'>{t('greeting')}</Typography>
+    </>
+  )
 }
 
 Page.leftActionType = 'menu'
